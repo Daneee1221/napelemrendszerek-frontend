4756be149b3542035ee3a376a242ccb9e9488314
Anna Németh <anna.tordemic@gmail.com>
2023-03-08 09:51:54 +0100

    🧱 Moved DB Classes from backend

---
+++ napelemrendszerek-frontend/DBClasses/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace napelemrendszerek_backend.Models
+{
+    public partial class Users
+    {
+        public string Username { get; set; }
+        public string UserPassword { get; set; }
+        public int RoleId { get; set; }
+    }
+}
