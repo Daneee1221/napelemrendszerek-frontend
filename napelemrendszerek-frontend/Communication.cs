43a06374c0e4b4089d0e1e89b24469df063a22cd
Ábel <sabel0929@gmail.com>
2023-03-12 21:22:50 +0100

    🧱 Changed Comm. content from single object to list

--- napelemrendszerek-frontend/Communication.cs
+++ napelemrendszerek-frontend/Communication.cs
@@ -12,18 +12,28 @@ namespace Comm
     {
         public string Message { get; set; }
         public DateTime Date { get; set; }
-        public Dictionary<string, string> Content { get; set; }
+        public List<Dictionary<string, string>> Content { get; set; }
         public int? RoleId { get; set; }
 
-        public Communication() { }
-        public Communication(string message, Dictionary<string, string> content, int roleId)
+        public Communication()
+        {
+            Message = String.Empty;
+            Content = new List<Dictionary<string, string>>();
+            Date = DateTime.Now;
+        }
+        public Communication(string message, List<Dictionary<string, string>> content, int roleId)
         {
             this.Message = message;
             this.Date = DateTime.Now;
             this.Content = content;
             this.RoleId = roleId;
         }
-        
+
+        public void addItemToContent(Dictionary<string, string> newElement)
+        {
+            Content.Add(newElement);
+        }
+
         public override string ToString()
         {
             return Message + "[" + Date.ToString() + "]";
