dec365875d253547042980235500c2e05ce28d29
Ábel <sabel0929@gmail.com>
2023-03-16 08:58:00 +0100

    ✨Added logic to from controls

--- napelemrendszerek-frontend/Raktarvezeto_PartManagementPage.xaml.cs
+++ napelemrendszerek-frontend/Raktarvezeto_PartManagementPage.xaml.cs
@@ -27,9 +27,82 @@ namespace napelemrendszerek_frontend
             List<Part> parts = ((MainWindow)Application.Current.MainWindow).StartGetPartsProcess();
             LB_Parts.DataContext = parts;
         }
+
         private void LB_Parts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SP_ModifyPartData.DataContext = LB_Parts.SelectedItem;
+        }
+
+        private void BTN_ClearModifyForm_Click(object sender, RoutedEventArgs e)
+        {
+            LB_Parts.SelectedIndex = -1;
+            TB_MaxNumberInBox.Clear();
+            TB_SellPrice.Clear();
+        }
+
+        private void BTN_SaveNewPart_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_NewMaxNumberInBox.Text == "")
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+            if (TB_NewPartName.Text == "")
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+            if (TB_NewSellPrice.Text == "")
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+            Part newPart = new Part(TB_NewPartName.Text, Convert.ToInt32(TB_NewMaxNumberInBox.Text), Convert.ToInt32(TB_NewSellPrice.Text), 0);
+            //TODO: Küldés a backendnek
+        }
+
+        private void BTN_ClearNewPartForm_Click(object sender, RoutedEventArgs e)
+        {
+            TB_NewPartName.Clear();
+            TB_NewMaxNumberInBox.Clear();
+            TB_NewSellPrice.Clear();
+        }
+
+        private void BTN_ModifyPart_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_MaxNumberInBox.Text == "")
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+            if (TB_SellPrice.Text == "")
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+            if (LB_Parts.SelectedIndex == -1)
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+
+            Part selectedPart = LB_Parts.SelectedItem as Part;
+            Dictionary<string, string> newValues = new Dictionary<string, string>();
+            if (selectedPart.SellPrice != Convert.ToInt32(TB_SellPrice))
+            {
+                newValues.Add("SellPrice", TB_SellPrice.Text);
+            }
+            if (selectedPart.MaxNumberInBox != Convert.ToInt32(TB_MaxNumberInBox.Text))
+            {
+                newValues.Add("MaxNumberInBox", TB_MaxNumberInBox.Text);
+            }
+            if (newValues.Count == 0)
+            {
+                //TODO: Hiba jelzés
+                return;
+            }
+
+            //TODO: küldés backendre
         }
     }
 }
