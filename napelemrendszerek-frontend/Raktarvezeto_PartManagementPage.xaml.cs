2182b8a49d312fb119ce4d3d37efa3344ccf6144
Ábel <sabel0929@gmail.com>
2023-03-16 08:50:46 +0100

    ✨ Added part management UI for Raktarvezeto

---
+++ napelemrendszerek-frontend/Raktarvezeto_PartManagementPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using napelemrendszerek_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace napelemrendszerek_frontend
+{
+    /// <summary>
+    /// Interaction logic for Raktarvezeto_PartManagementPage.xaml
+    /// </summary>
+    public partial class Raktarvezeto_PartManagementPage : Page
+    {
+        public Raktarvezeto_PartManagementPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
